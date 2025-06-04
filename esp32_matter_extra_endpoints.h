fe5d9357194b26559f9148e2e1804d3ddd0c32c2
Jan Reiner <100093129+EatingJan1@users.noreply.github.com>
2025-06-04 20:06:26 +0200

    esp32_matter_extra_endpoints

---
+++ esp32_matter_extra_endpoints.h
@@ -0,0 +1,9 @@
+#ifndef ESP32_MATTER_EXTRA_ENDPOINTS_H
+#define ESP32_MATTER_EXTRA_ENDPOINTS_H
+
+#include "src/MatterAirQualitySensor.h"
+#include "src/MatterRainSensor.h"
+#include "src/MatterAmbientLightSensor.h"
+#include "src/MatterFlowSensor.h"
+
+#endif
